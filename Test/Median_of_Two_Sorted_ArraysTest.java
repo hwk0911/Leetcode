e8c6db06bb4306dfe94beb18db1e17f4dae348dc
김현우 <hwk0911@gmail.com>
2021-01-14 20:02:08 +0900

    3.Median_of_Two_Sorted_Arrays solution & Testcode
    [Wrong exceptions]

---
+++ Test/Median_of_Two_Sorted_ArraysTest.java
@@ -0,0 +1,37 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Median_of_Two_Sorted_ArraysTest {
+
+    Median_of_Two_Sorted_Arrays main = new Median_of_Two_Sorted_Arrays();
+
+    @Test
+    void findMedianSortedArrays() {
+        int[] nums1_1 = {1,3};
+        int[] nums2_1 = {2};
+        double answer_1 = 2f;
+
+        int[] nums1_2 = {1,2};
+        int[] nums2_2 = {3,4};
+        double answer_2 = 2.5f;
+
+        int[] nums1_3 = {2};
+        int[] nums2_3 = {};
+        double answer_3 = 2f;
+
+        int[] nums1_4 = {};
+        int[] nums2_4 = {1};
+        double answer_4 = 1f;
+
+        int[] nums1_5 = {1, 3, 5, 7};
+        int[] nums2_5 = {2, 4, 6, 8};
+        double answer_5 = 4.5f;
+
+        assertEquals(main.findMedianSortedArrays(nums1_1, nums2_1), answer_1);
+        assertEquals(main.findMedianSortedArrays(nums1_2, nums2_2), answer_2);
+        assertEquals(main.findMedianSortedArrays(nums1_3, nums2_3), answer_3);
+        assertEquals(main.findMedianSortedArrays(nums1_4, nums2_4), answer_4);
+        assertEquals(main.findMedianSortedArrays(nums1_5, nums2_5), answer_5);
+    }
+}
---
+++ src/Median_of_Two_Sorted_Arrays.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Median_of_Two_Sorted_Arrays {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        List<Double> mergeArray = new ArrayList<>();
+
+        for (int num : nums1) {
+            mergeArray.add((double)num);
+        }
+
+        for (int num : nums2) {
+            mergeArray.add((double)num);
+        }
+
+        Collections.sort(mergeArray);
+
+        if (mergeArray.size() % 2 == 0) {
+            int median = mergeArray.size() / 2;
+
+            return (mergeArray.get(median) + mergeArray.get(median - 1)) / 2;
+        }
+        else {
+            return mergeArray.get(mergeArray.size() / 2);
+        }
+    }
+}
