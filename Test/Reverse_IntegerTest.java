160a2ab0546cd2291eb1163001aeb305b2c30df8
김현우 <hwk0911@gmail.com>
2021-01-18 22:20:46 +0900

    6.ZigZag Conversion solution & Testcode
    [Wrong exceptions]
    1. I can't cope the NumberFormatException. the answer number have the possibility of bigger than 32-bit integer size

---
+++ Test/Reverse_IntegerTest.java
@@ -0,0 +1,28 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Reverse_IntegerTest {
+
+    Reverse_Integer main = new Reverse_Integer();
+
+    @Test
+    void reverse() {
+        int x_1 = 123;
+        int answer_1 = 321;
+
+        int x_2 = -123;
+        int answer_2 = -321;
+
+        int x_3 = 120;
+        int answer_3 = 21;
+
+        int x_4 = 0;
+        int answer_4 = 0;
+
+        assertEquals(main.reverse(x_1), answer_1);
+        assertEquals(main.reverse(x_2), answer_2);
+        assertEquals(main.reverse(x_3), answer_3);
+        assertEquals(main.reverse(x_4), answer_4);
+    }
+}
---
+++ src/Reverse_Integer.java
@@ -0,0 +1,25 @@
+public class Reverse_Integer {
+    public int reverse(int x) {
+        StringBuilder sb = new StringBuilder();
+
+        if (x < 0) {
+            sb.append('-');
+            x *= -1;
+        }
+
+        do {
+            sb.append(x % 10);
+            x /= 10;
+        }while (x != 0);
+
+        int answer;
+
+        try {
+            answer = Integer.parseInt(sb.toString());
+        } catch (NumberFormatException e) {
+            answer = 0;
+        }
+
+        return answer;
+    }
+}
